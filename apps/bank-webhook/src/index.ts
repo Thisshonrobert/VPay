43ee701658a72821c5929df4665df441f243461e
ディション・ロバート <thisshonrobert0205@gmail.com>
2025-11-21 16:15:59 +0530

    added webhook for zap

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
- import  db from "@repo/db/client";
+import db from "@repo/db/client";
 
 import swaggerJSDoc from "swagger-jsdoc";
 import { z } from "zod";
 import swaggerUi from 'swagger-ui-express';
+import dotenv from "dotenv";
+dotenv.config();
 
 // const db = require("@repo/db/client")
 const app = express();
@@ -11,15 +13,15 @@ const app = express();
 app.use(express.json())
 
 const swaggerSpec = swaggerJSDoc({
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "VPay Bank Webhook API",
-      version: "1.0.0",
-      description: "Swagger documentation for bank webhook - Can hit /hdfcWebhook endpoint instead of actual bank for testing",
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "VPay Bank Webhook API",
+            version: "1.0.0",
+            description: "Swagger documentation for bank webhook - Can hit /hdfcWebhook endpoint instead of actual bank for testing",
+        },
     },
-  },
-  apis: ["./src/index.ts"], // Or wherever you keep your routes/controllers
+    apis: ["./src/index.ts"], // Or wherever you keep your routes/controllers
 });
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -27,7 +29,7 @@ const paymentSchema = z.object({
     token: z.string(),
     user_identifier: z.string(),
     amount: z.string(),
-    PaymentResponse:z.enum(["Success", "Failure"])
+    PaymentResponse: z.enum(["Success", "Failure"])
 });
 /**
  * @swagger
@@ -60,9 +62,9 @@ const paymentSchema = z.object({
  */
 
 app.post("/hdfcWebhook", async (req, res) => {
-    
+
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-   
+
 
     // Zod validation
     const validation = paymentSchema.safeParse(req.body);
@@ -72,7 +74,7 @@ app.post("/hdfcWebhook", async (req, res) => {
             errors: validation.error.errors
         });
     }
-enum PaymentResponse {
+    enum PaymentResponse {
         Success = "Success",
         Failure = "Failure"
     }
@@ -88,9 +90,9 @@ enum PaymentResponse {
         amount: validation.data.amount,
         PaymentResponse: validation.data.PaymentResponse === "Success" ? PaymentResponse.Success : PaymentResponse.Failure
     };
-   
+
     try {
-        if(paymentInformation.PaymentResponse !== PaymentResponse.Success) {
+        if (paymentInformation.PaymentResponse !== PaymentResponse.Success) {
             await db.$transaction([
                 db.balance.updateMany({
                     where: {
@@ -115,39 +117,58 @@ enum PaymentResponse {
                 message: "Error while processing webhook: No records updated"
             });
         }
-    
-        else{
-         const [balanceUpdate, transactionUpdate] =  await db.$transaction([
-            db.balance.updateMany({
-                where: {
-                    userId: Number(paymentInformation.userId)
-                },
-                data: {
-                    amount: {
-                        increment: Number(paymentInformation.amount)
+
+        else {
+            const [balanceUpdate, transactionUpdate] = await db.$transaction([
+                db.balance.updateMany({
+                    where: {
+                        userId: Number(paymentInformation.userId)
                     },
-                    locked:{
-                        decrement:Number(paymentInformation.amount)
+                    data: {
+                        amount: {
+                            increment: Number(paymentInformation.amount)
+                        },
+                        locked: {
+                            decrement: Number(paymentInformation.amount)
+                        }
                     }
-                }
-            }),
-            db.onRampTransaction.updateMany({
-                where: {
-                    token: paymentInformation.token
-                }, 
-                data: {
-                    status: "Success",
-                }
-            })
-        ]);
-         console.log(balanceUpdate, transactionUpdate);
+                }),
+                db.onRampTransaction.updateMany({
+                    where: {
+                        token: paymentInformation.token
+                    },
+                    data: {
+                        status: "Success",
+                    }
+                })
+            ]);
+            try {
+                await axios.post(
+                    "http://localhost:3002/hooks/catch/1/41ff9d05-7a20-41e3-9874-c0e6ecc6450b",
+                    {
+                        from: "thisshonrobert0205@gmail.com",
+                        to: "thisshonrobert0205@gmail.com",
+                        subject: "Vpay Wallet credited",
+                        body: `Vpay ₹${paymentInformation.amount} added to your wallet`
+                    },
+                    {
+                        headers: {
+                            "X-ZAP-SECRET": process.env.ZAP_SECRET!,
+                        }
+                    }
+                );
+
+            } catch (error) {
+                console.error("Failed to send email notification: through Zap", error);
+            }
+            console.log(balanceUpdate, transactionUpdate);
 
 
-        res.json({
-            message: "Captured"
-        });
-    }
-    } catch(e) {
+            res.json({
+                message: "Captured"
+            });
+        }
+    } catch (e) {
         console.error(e);
         // On failure, update transaction status to 'Failed' and remove locked amount
         try {
@@ -181,8 +202,8 @@ enum PaymentResponse {
 
 })
 app.listen(3003, () => {
-  console.log("Server running at http://localhost:3003");
-  console.log("Docs available at http://localhost:3003/api-docs");
+    console.log("Server running at http://localhost:3003");
+    console.log("Docs available at http://localhost:3003/api-docs");
 });
 
 
